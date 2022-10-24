005eacaeb0c6da4cd788ce9c9b3442ed6669b435
vbg <vbg911@gmail.com>
2022-10-24 21:08:32 +0300

    added new code. 2 of 3 successful unit test.

--- delete.py
+++ delete.py
@@ -2,8 +2,35 @@ import os
 import shutil
 
 def delete(file_path):
-    return
+    is_file = isFile(file_path)
+    if is_file:
+        return delete_file(file_path)
 
+    return False
+
+
+def isFile(file_path):
+    return os.path.isfile(file_path)
+
+
+def delete_file(file_path):
+    with open(file_path, "wb") as f:
+        f.write(bytes('qQq', 'utf8'))
+        f.close()
+
+    with open(file_path, "wb") as f:
+        f.write(bytes('AaA', 'utf8'))
+        f.close()
+
+    with open(file_path, "wb") as f:
+        f.write(bytes('PpP', 'utf8'))
+        f.close()
+
+    os.remove(file_path)
+    if os.path.exists(file_path):
+        return False
+    else:
+        return True
 
 if __name__ == '__main__':
     file_path = input("Введите путь к файлу/папке: ")
