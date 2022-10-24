c882c421c77993cb413151e2bdbedde1f36784cc
vbg <vbg911@gmail.com>
2022-10-24 21:10:47 +0300

    added new code. 3 of 3 successful unit test.

--- delete.py
+++ delete.py
@@ -2,11 +2,19 @@ import os
 import shutil
 
 def delete(file_path):
+    is_folder = isFolder(file_path)
     is_file = isFile(file_path)
     if is_file:
         return delete_file(file_path)
 
+    if is_folder:
+        return delete_folder(file_path)
+
     return False
+
+
+def isFolder(file_path):
+    return os.path.isdir(file_path)
 
 
 def isFile(file_path):
@@ -27,6 +35,18 @@ def delete_file(file_path):
         f.close()
 
     os.remove(file_path)
+    if os.path.exists(file_path):
+        return False
+    else:
+        return True
+
+
+def delete_folder(file_path):
+    arr = os.listdir(file_path)
+    for item in arr:
+        if isFile(item):
+            delete_file(item)
+    shutil.rmtree(file_path)
     if os.path.exists(file_path):
         return False
     else:
